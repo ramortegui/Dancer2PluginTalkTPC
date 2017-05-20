2d6433e09809ef4ae5a8424d1148a950c99e69cb
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-20 14:03:19 -0600

    Add minimun data for products.

--- samples/D2PC_Demo/lib/D2PC_Demo.pm
+++ samples/D2PC_Demo/lib/D2PC_Demo.pm
@@ -8,4 +8,13 @@ get '/' => sub {
     template 'index' => { 'title' => 'D2PC_Demo' };
 };
 
+hook 'plugin.cart.products' => sub {
+  my $ec_cart = session('ec_cart');
+  $ec_cart->{products} = [
+    { ec_sku => "book1", ec_price => 10 },
+    { ec_sku => "book2", ec_price => 15 },
+  ];
+  session "ec_cart" => $ec_cart;
+};
+
 true;
