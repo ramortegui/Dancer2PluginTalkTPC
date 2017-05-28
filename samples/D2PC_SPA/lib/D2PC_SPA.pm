54c54ad2fb2340b534972cc3d1afd03fd4dde779
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-27 22:22:14 -0600

    Add json calls API to manage cart.

--- samples/D2PC_SPA/lib/D2PC_SPA.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA.pm
@@ -5,16 +5,7 @@ use Dancer2::Plugin::Cart;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'D2PC_SPA', 'product_list' => products };
-};
-
-hook 'plugin.cart.products' => sub {
-  my $ec_cart = session('ec_cart');
-  $ec_cart->{products} = [
-    { ec_sku =>'SU01',ec_price => 10 },
-    { ec_sku =>'SU02',ec_price => 15 },
-  ];
-  session "ec_cart" => $ec_cart;
+  template 'index' => { 'title' => 'D3PC_SPA', 'product_list' => products };
 };
 
 true;
--- samples/D2PC_SPA/lib/D2PC_SPA/API.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA/API.pm
@@ -1,9 +1,16 @@
 package D2PC_SPA::API;
 use Dancer2;
 use Dancer2::Plugin::Cart;
-set serializer => 'JSON';
+set serializer => "JSON";
 
-
+hook 'plugin.cart.products' => sub {
+  my $ec_cart = session('ec_cart');
+  $ec_cart->{products} = [
+    { ec_sku =>'SU01',ec_price => 10 },
+    { ec_sku =>'SU02',ec_price => 15 },
+  ];
+  session "ec_cart" => $ec_cart;
+};
 get '/products' => sub {
   products
 };
@@ -13,7 +20,8 @@ get '/cart' => sub {
 };
 
 post '/cart/add' => sub {
-  cart_add
+  my $params = params;
+  cart_add_item($params);
   cart
 };
 
