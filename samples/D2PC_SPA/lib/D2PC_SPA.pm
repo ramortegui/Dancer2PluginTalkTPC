d64395e38a347e75ba52da13c054749605a939e4
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-27 19:06:57 -0600

    Update index page, to show product list.

--- samples/D2PC_SPA/lib/D2PC_SPA.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA.pm
@@ -5,7 +5,16 @@ use Dancer2::Plugin::Cart;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'D2PC_SPA' };
+    template 'index' => { 'title' => 'D2PC_SPA', 'product_list' => products };
+};
+
+hook 'plugin.cart.products' => sub {
+  my $ec_cart = session('ec_cart');
+  $ec_cart->{products} = [
+    { ec_sku =>'SU01',ec_price => 10 },
+    { ec_sku =>'SU02',ec_price => 15 },
+  ];
+  session "ec_cart" => $ec_cart;
 };
 
 true;
--- samples/D2PC_SPA/lib/D2PC_SPA/API.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA/API.pm
@@ -3,14 +3,6 @@ use Dancer2;
 use Dancer2::Plugin::Cart;
 set serializer => 'JSON';
 
-hook 'plugin.cart.products' => sub {
-  my $ec_cart = session('ec_cart');
-  $ec_cart->{products} = [
-    { ec_sku =>'SU01',ec_price => 10 },
-    { ec_sku =>'SU02',ec_price => 15 },
-  ];
-  session "ec_cart" => $ec_cart;
-};
 
 get '/products' => sub {
   products
