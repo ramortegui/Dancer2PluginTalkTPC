2222222a83be0bd5dc08f26a78fe2fca59c7a84e
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-06-03 17:26:55 -0600

    Load and render products using vue.

--- samples/D2PC_SPA/app/initialize.js
+++ samples/D2PC_SPA/app/initialize.js
@@ -4,8 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 import Vue from 'vue';
 new Vue({
-  el: "#hello-world",
+  el: "#products",
   data: {
-    message: "Hello World"
+    products: [{
+      ec_sku: "su01",
+      ec_price: 10
+    }] 
+  },
+  created: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      var xhr = new XMLHttpRequest()
+        var self = this
+        xhr.open('GET', '/api/products')
+        xhr.onload = function () {
+          self.products = JSON.parse(xhr.responseText)
+            console.log(self.products)
+        }
+      xhr.send()
+    }
+  },
+  components: {
+    product:{
+      template: "#template_product",
+      props: ["x"],
+      replace: false
+    }
   }
 });
