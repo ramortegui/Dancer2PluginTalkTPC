1e3df559f9c3b2f1bfd132728a61821e9d2975a9
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-06-03 00:10:06 -0600

    Add vue to dependencies.

--- samples/D2PC_SPA/app/initialize.js
+++ samples/D2PC_SPA/app/initialize.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
 });
+import Vue from 'vue';
+new Vue({
+  el: "#hello-world",
+  data: {
+    message: "Hello World"
+  }
+});
--- samples/D2PC_SPA/brunch-config.js
+++ samples/D2PC_SPA/brunch-config.js
@@ -1,14 +1,16 @@
 // See http://brunch.io for documentation.
+//
 exports.files = {
-  javascripts: {
-    joinTo: {
-      'javascripts/vendor.js': /^(?!app)/, // Files that are not in `app` dir.
-      'javascripts/app.js': /^app/
-    }
-  },
+  javascripts: { joinTo: 'js/app.js' },
   stylesheets: {joinTo: 'css/app.css'}
 };
 
 exports.plugins = {
   babel: {presets: ['latest']}
+};
+
+exports.npm = {
+  aliases: {
+    vue: "vue/dist/vue.js" 
+  }
 };
