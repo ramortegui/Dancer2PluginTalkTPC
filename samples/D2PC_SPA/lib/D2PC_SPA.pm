2fb8363c851910e8724fa600db552a9d7b1cdafc
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-06-20 00:12:52 -0600

    Remove Dancer2::Plugin::Cart from main app.

--- samples/D2PC_SPA/lib/D2PC_SPA.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA.pm
@@ -1,11 +1,10 @@
 package D2PC_SPA;
 use Dancer2;
-use Dancer2::Plugin::Cart;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-  template 'index' => { 'title' => 'D3PC_SPA', 'product_list' => products };
+  template 'index' => { 'title' => 'D3PC_SPA' };
 };
 
 true;
