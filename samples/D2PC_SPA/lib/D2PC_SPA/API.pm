954a9153305ce333b2fc965eaab815f4f5184bea
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-06-20 11:34:40 -0600

    Fix typos.

--- samples/D2PC_SPA/lib/D2PC_SPA/API.pm
+++ samples/D2PC_SPA/lib/D2PC_SPA/API.pm
@@ -6,10 +6,10 @@ set serializer => "JSON";
 
 hook 'plugin.cart.products' => sub {
   my $ec_cart = session('ec_cart');
-  @{$ec_cart->{products}} = {
+  @{$ec_cart->{products}} = (
     { ec_sku =>'CN01', ec_price => 50, description => "Consulting"  },
     { ec_sku =>'TR01', ec_price => 115, description => "Training" },
-  };
+  );
   session "ec_cart" => $ec_cart;
 };
 
